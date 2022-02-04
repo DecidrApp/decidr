ce559337728204849a96d967d13bb5e4121997cf
JoeZlonicky <joe.zlonicky@gmail.com>
2022-02-03 16:36:32 -0800

    Made bare-bones title screen

--- App.js
+++ App.js
@@ -1,111 +1,90 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={[
+        styles.background,
+        {
+          backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        },
+      ]}>
+      <View>
+        <Text
+          style={[
+            styles.title,
+            {
+              color: isDarkMode ? Colors.white : Colors.black,
+            },
+          ]}>
+          {'Decidr'}
+        </Text>
+
+        <TouchableHighlight onPress={() => {}} style={styles.button} activeOpacity={0.6} underlayColor={'#00AA00'}>
+          <Text
+            style={[
+              styles.buttonText,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>
+            {'Host Room'}
+          </Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight onPress={() => {}} style={styles.button} activeOpacity={0.6} underlayColor={'#00AA00'}>
+          <Text
+            style={[
+              styles.buttonText,
+              {
+                color: isDarkMode ? Colors.white : Colors.black,
+              },
+            ]}>
+            {'Join Room'}
+          </Text>
+        </TouchableHighlight>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  background: {
+    flex: 1,
+    flexGrow: 10,
+    paddingTop: 150,
+    paddingLeft: 50,
+    paddingRight: 50,
   },
-  sectionTitle: {
-    fontSize: 24,
+  title: {
+    fontSize: 48,
     fontWeight: '600',
+    textAlign: 'center',
+    marginBottom: 50,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#8e208e',
+    padding: 20,
+    marginBottom: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  buttonText: {
+    fontSize: 32,
   },
 });
 
